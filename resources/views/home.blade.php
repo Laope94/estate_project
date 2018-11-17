2f8b35e93cf6a4ea55c49e7eef0eb715ce3e7ff3
Laope94 <timotej.sulka@gmail.com>
2018-11-17 02:06:49 +0100

    front end - zmeny na home stránke, pridané logá, zmena layoutu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,25 +2,35 @@
 @section('title', 'Domov')
 @section('menu')
     @parent
-<div style="background: #F6F6F6;">
-    <div class="intro-page-container">
-        <div class="intro-title-wrapper">
-        <h1 class="intro-title">Nájdeme pre Vás to pravé.</h1>
+    <div style="background: #F6F6F6;">
+
+            <div class="intro-title-wrapper">
+                <h1 class="intro-title">Nájdeme pre Vás to pravé.</h1>
+            </div>
+            <div class="chevron-circle-container"><i class="fas fa-chevron-circle-down arrow-down"></i></div>
+
+        <div style="height: 90%;"><h1 style="text-align: center">U nás nájdete</h1>
+            <div>
+                <i class="fab fa-connectdevelop awesome-about"></i> <br> 100k+ inzercií
+            </div>
+            <div>
+                <i class="fas fa-users awesome-about"></i> <br> 30k+ návštevníkov
+            </div>
+            <div>
+                <i class="fas fa-city awesome-about"></i> <br> 1000k+ realitných kancelárií
+            </div>
+            <div>
+                <i class="fas fa-user-tie awesome-about"></i> <br> 10k+ realitných agentov
+            </div>
         </div>
-    </div>
-    <div style="height: 90%;"><h1 style="text-align: center">U nás nájdete</h1>
-        100k+ inzercií <br>
-        30k+ návštevníkov <br>
-        1000k+ realitných kancelárií <br>
-        10k+ realitných agentov <br>
-    </div>
-    <div class="quick-search-container">
-        <h1 class="search-bar-title">Rýchle vyhľadávanie</h1>
-        <input class="quick-search-bar" type="text" placeholder="Zadaj okres..." onfocus="this.placeholder=''" onblur="this.placeholder='Zadaj okres...'" style="width: 80%;">
+        <div class="quick-search-container">
+            <h1 class="search-bar-title">Rýchle vyhľadávanie</h1>
+            <input class="quick-search-bar" type="text" placeholder="Zadaj okres..." onfocus="this.placeholder=''"
+                   onblur="this.placeholder='Zadaj okres...'" style="width: 80%;">
+        </div>
+
+        <div style="height: 33%;"><h1 style="text-align: center">Najnovšie inzeráty</h1></div>
     </div>
 
-    <div style="height: 33%;"><h1 style="text-align: center">Najnovšie inzeráty</h1></div>
-</div>
 
-
-    @endsection
+@endsection
--- resources/views/layouts/master_layout.blade.php
+++ resources/views/layouts/master_layout.blade.php
@@ -1,39 +1,50 @@
 <header>
-<title>bytvdome.sk | @yield('title') </title>
+    <title>bytvdome.sk | @yield('title') </title>
     <link href="{{asset('/css/public_pages.css')}}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&amp;subset=latin-ext" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
+          integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="shortcut icon" href="{{asset('/favicon.ico')}}">
 </header>
 <body>
 <div>
-
+    <div class="web-logo-container"><img class="web-logo" src="{{asset('/images/web-logo.png')}}"></div>
     <div class="nav-container">
         @section('menu')
-    <nav>
-        <a class="nav-link" href="">Domov</a>
-        <a class="nav-link" href="">Vyhľadať nehnuteľnosť</a>
-        <a class="nav-link" href="">Realitné kancelárie</a>
-        <a class="nav-link" href="">Registrácia</a>
-        <a class="nav-link" href="">Kontakt</a>
-    </nav>
+            <nav>
+                <a class="nav-link" href="">Domov</a>
+                <a class="nav-link" href="">Vyhľadať nehnuteľnosť</a>
+                <a class="nav-link" href="">Realitné kancelárie</a>
+                <a class="nav-link" href="">Registrácia</a>
+                <a class="nav-link" href="">Kontakt</a>
+            </nav>
     </div>
-        @show
+    <div class="intro-page-container"></div>
+    @show
 
-    </div>
+</div>
 </body>
 <footer>
     <div class="upper-footer">
         <div class="upper-footer-container">
-        ©2018 bytvdome.sk<br>
-        Všetky práva vyhradené.
+            ©2018 bytvdome.sk<br>
+            Všetky práva vyhradené.
         </div>
         <div class="footer-social-container">
             Sledujte nás aj na sociálnych sieťach.<br>
             <div class="round-button-container">
-                <a href=""><div class="round-button"><i class="fab fa-facebook-f awesome-social"></i></div></a>
-                <a href=""><div class="round-button"><i class="fab fa-instagram awesome-social"></i></div></a>
-                <a href=""><div class="round-button"><i class="fab fa-twitter awesome-social"></i></div></a>
-                <a href=""><div class="round-button"><i class="fab fa-google-plus-g awesome-social"></i></div></a>
+                <a href="">
+                    <div class="round-button"><i class="fab fa-facebook-f awesome-social"></i></div>
+                </a>
+                <a href="">
+                    <div class="round-button"><i class="fab fa-instagram awesome-social"></i></div>
+                </a>
+                <a href="">
+                    <div class="round-button"><i class="fab fa-twitter awesome-social"></i></div>
+                </a>
+                <a href="">
+                    <div class="round-button"><i class="fab fa-google-plus-g awesome-social"></i></div>
+                </a>
             </div>
         </div>
     </div>
@@ -44,7 +55,7 @@
     <div class="lower-footer">
         <div class="footer-unicorn">
             <span class="unicorn-created">created by</span>
-            <span>LOGO</span>
+            <img class="unicorn-logo" src="{{ asset('/images/unicorn.png') }}">
             <span class="unicorn-sh">Unicorn Software House</span>
         </div>
     </div>
