7d1746bbb46ff3d44ee13bf0404be07270499fef
mv280 <mirovirag280@gmail.com>
2018-11-28 11:37:44 +0100

    s

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,6 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'surname' => $data['surname'],
-            'city' => $data['city'],
             'address' => $data['street'],
             'email' => $data['email'],
             'phone' => $data['phone_prim'],
